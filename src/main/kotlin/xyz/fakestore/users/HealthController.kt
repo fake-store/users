383672506b5e02f638a1608c5eb2dadbff7d0599
brianbcampbell <52678934+brianbcampbell@users.noreply.github.com>
2026-03-17 16:50:11 -0600

    Add /health endpoint and multi-arch Docker builds

---
+++ src/main/kotlin/xyz/fakestore/users/HealthController.kt
@@ -0,0 +1,11 @@
+package xyz.fakestore.users
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HealthController {
+
+    @GetMapping("/health")
+    fun health() = mapOf("status" to "healthy")
+}
